b72b94f90edd5b3af54eb27d4d7f2859f2707aaf
AKIMANA Gabriel <105674142+akimanagabriel@users.noreply.github.com>
2023-07-22 13:22:49 +0200

    initial commit

---
+++ src/api.js
@@ -0,0 +1,10 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "430adb4fdbmshd6a23c1be12cf3cp15d213jsn40b9d1fbea48",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d32c2faf3cb8c4b5ad7bd07a7453a479"; 
